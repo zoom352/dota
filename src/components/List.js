565e0fa801a0d46fcec3be2625342ef28631a20c
Dmitry Kulinich <dimakul.spb@inbox.ru>
2023-05-29 14:24:57 +0300

    fix styles

--- src/components/List.js
+++ src/components/List.js
@@ -16,9 +16,9 @@ const List = () => {
     };
 
     const transitions = useTransition(list, {
-        from: { opacity: 0, transform: 'translateX(-200%)', width: '0%', paddingBottom: '0%' },
-        enter: { opacity: 1, transform: 'translateX(0%)', width: '20%', paddingBottom: '20%' },
-        leave: { opacity: 0, transform: 'translateX(200%)', width: '0%', paddingBottom: '0%' },
+        from: { opacity: 0, transform: 'translateX(-100%)' },
+        enter: { opacity: 1, transform: 'translateX(0%)' },
+        leave: { opacity: 0, transform: 'translateX(100%)' },
     })
 
     return (
@@ -27,13 +27,16 @@ const List = () => {
                 <button onClick={handleAddItem}>Добавить</button>
                 <button onClick={handleRemoveItem}>Удалить</button>
             </div>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div style={{ display: 'flex', overflow: 'hidden' }}>
                 {transitions((style, item) => (
                     <animated.div
                         key={item.id}
                         style={{
                             ...style,
+                            width: '20%',
                             backgroundColor: item.color,
+                            height: '0',
+                            paddingBottom: '20%'
                         }}
                     />
                 ))}
